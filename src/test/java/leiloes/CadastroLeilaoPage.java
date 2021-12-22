5f7c26479c45700e490da77c8a9d7568a09c5cf9
gustavodias <gustavoemersonfd@gmail.com>
2021-12-22 13:05:05 -0300

    Testando a validação dos campos do cadastro de um Leilao;

--- src/test/java/leiloes/CadastroLeilaoPage.java
+++ src/test/java/leiloes/CadastroLeilaoPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class CadastroLeilaoPage {
 
+    public static final String URL_CADASTRO_LEILAO="http://localhost:8080/leiloes/new";
     private final WebDriver browser;
 
     public CadastroLeilaoPage(WebDriver browser) {
@@ -21,5 +22,17 @@ public class CadastroLeilaoPage {
         this.browser.findElement(By.id("dataAbertura")).sendKeys(dataAbertura);
         this.browser.findElement(By.id("button-submit")).submit();
         return new LeiloesPage(browser);
+    }
+
+    public boolean isPaginaCadLeilao() {
+        return this.browser.getCurrentUrl().equals(URL_CADASTRO_LEILAO);
+    }
+
+    public boolean isMensageDeValidacaoVisiveis() {
+        String pageSource = browser.getPageSource();
+        return pageSource.contains("minimo 3 caracteres") &&
+                pageSource.contains("não deve estar em branco")&&
+                pageSource.contains("deve ser um valor maior de 0.1")&&
+                pageSource.contains("deve ser uma data no formato dd/MM/yyyy");
     }
 }
--- src/test/java/leiloes/LeiloesTest.java
+++ src/test/java/leiloes/LeiloesTest.java
@@ -12,6 +12,16 @@ import java.time.format.DateTimeFormatter;
 class LeiloesTest {
 
     private LeiloesPage leiloesPage;
+    private CadastroLeilaoPage cadastroLeilaoPage;
+
+    @BeforeEach
+    void beforeEach(){
+        LoginPage loginPage = new LoginPage();
+        loginPage.preencheFormularioDeLogin("fulano", "pass");
+        this.leiloesPage = loginPage.efetuarLogin();
+        leiloesPage.carregarFormulario();
+        this.cadastroLeilaoPage = leiloesPage.carregarFormulario();
+    }
 
     @AfterEach
     void afterEach(){
@@ -20,12 +30,6 @@ class LeiloesTest {
 
     @Test
     void deveriaCadastrarLeilao(){
-        LoginPage loginPage = new LoginPage();
-        loginPage.preencheFormularioDeLogin("fulano", "pass");
-        this.leiloesPage = loginPage.efetuarLogin();
-        leiloesPage.carregarFormulario();
-        CadastroLeilaoPage cadastroLeilaoPage =leiloesPage.carregarFormulario();
-
         String hoje = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         String nome = "Leilao do dia "+hoje;
         String valor = "500.00";
@@ -33,5 +37,14 @@ class LeiloesTest {
         this.leiloesPage = cadastroLeilaoPage.cadastrarLeilao(nome,valor,hoje);
 
         Assert.assertTrue(leiloesPage.isLeilaoCadastrado(nome, valor, hoje));
+    }
+
+    @Test
+
+    void deveriaValidarCadastroLeilao(){
+        this.leiloesPage = cadastroLeilaoPage.cadastrarLeilao("","","");
+
+        Assert.assertFalse(this.cadastroLeilaoPage.isPaginaCadLeilao());
+        Assert.assertTrue(this.cadastroLeilaoPage.isMensageDeValidacaoVisiveis());
     }
 }
